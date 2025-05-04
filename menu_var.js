cc227f98c04d0bf8cd197b12456c1fe695255f54
Blaise Alicki <blaise@alicki.com>
2025-05-04 10:26:36 -0400

    Add files via upload

---
+++ menu_var.js
@@ -0,0 +1,86 @@
+	var NoOffFirstLineMenus=7;
+	var LowBgColor='#00CC00';
+	var LowSubBgColor='yellow';
+	var HighBgColor='black';
+	var HighSubBgColor='#00CC00';
+	var FontLowColor='black';
+	var FontSubLowColor='black';
+	var FontHighColor='#00CC00';
+	var FontSubHighColor='yellow';
+	var BorderColor='black';
+	var BorderSubColor='black';
+	var BorderWidth=1;
+	var BorderBtwnElmnts=1;
+	var FontFamily="arial,comic sans ms,technical";
+	var FontSize=9;
+	var FontBold=1;
+	var FontItalic=0;
+	var MenuTextCentered='left';
+	var MenuCentered='center';
+	var MenuVerticalCentered='top';
+	var ChildOverlap=.2;
+	var ChildVerticalOverlap=.2;
+	var StartTop=105;
+	var StartLeft=1;
+	var VerCorrect=0;
+	var HorCorrect=0;
+	var LeftPaddng=3;
+	var TopPaddng=2;
+	var FirstLineHorizontal=1;
+	var MenuFramesVertical=1;
+	var DissapearDelay=1000;
+	var TakeOverBgColor=1;
+	var FirstLineFrame='navig';
+	var SecLineFrame='space';
+	var DocTargetFrame='space';
+	var TargetLoc='';
+	var HideTop=0;
+	var MenuWrap=1;
+	var RightToLeft=0;
+	var UnfoldsOnClick=0;
+	var WebMasterCheck=0;
+	var ShowArrow=1;
+	var KeepHilite=1;
+	var Arrws=['tri.gif',5,10,'tridown.gif',10,5,'trileft.gif',5,10];
+
+function BeforeStart(){return}
+function AfterBuild(){return}
+function BeforeFirstOpen(){return}
+function AfterCloseAll(){return}
+
+Menu1=new Array("Site","","",2,20,90);
+	Menu1_1=new Array("Home Page","index.html","",0,20,110);
+	Menu1_2=new Array("Updates","updates.html","",0,20,110);
+
+Menu2=new Array("Me","","",4,20,90);
+	Menu2_1=new Array("About Me","about-me.html","",0,20,110);	
+	Menu2_2=new Array("My DVDs","my-dvds.html","",0,20,110);
+	Menu2_3=new Array("Ramblings","ramblings.html","",0,20,110);
+	Menu2_4=new Array("I'm Too Tired","im-too-tired.html","",0,20,110);
+
+Menu3=new Array("Art","","",5,20,90);
+	Menu3_1=new Array("Pitures","pitures.html","",0,20,110);	
+	Menu3_2=new Array("Drawlings","drawlings.html","",0,20,110);
+	Menu3_3=new Array("Colors","colors.html","",0,20,110);		
+	Menu3_4=new Array("Pixels","pixels.html","",0,20,110);	
+	Menu3_5=new Array("Comics","comics.html","",0,20,110);
+	Menu3_6=new Array("Comic Book","ninjahc.html","",0,20,110);
+
+Menu4=new Array("Other Works","","",3,20,90);
+	Menu4_1=new Array("Ode To My Pop","ode-to-my-pop.html","",0,20,110);
+	Menu4_2=new Array("Program Journal","program-journal.html","",0,20,110);
+	Menu4_3=new Array("Midi Song","blaise01.mid","",0,20,110);
+
+Menu5=new Array("Projects","","",4,20,90);
+	Menu5_1=new Array("Brilliant Ideas","brilliant-ideas.html","",0,20,110);
+	Menu5_2=new Array("i-Show","i-show.html","",0,20,110);
+	Menu5_3=new Array("Movies","movies.html","",0,20,110);
+	Menu5_4=new Array("Game","game.html","",0,20,110);
+
+Menu6=new Array("Kmart","","",2,20,90);
+	Menu6_1=new Array("Kmart","kmart.html","",0,20,110);
+	Menu6_2=new Array("Kmart Pics","kmart-pics.html","",0,20,110);
+
+Menu7=new Array("Interactive","","",2,20,90);
+	Menu7_1=new Array("Wallpapers","wallpapers.html","",0,20,110);
+	Menu7_2=new Array("Links","links.html","",0,20,110);
