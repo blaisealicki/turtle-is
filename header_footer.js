e85f1597bfba0275d394efb7f2c10d6667267b06
Blaise Alicki <blaise@alicki.com>
2025-05-04 12:46:29 -0400

    Style header and footer with css

--- header_footer.js
+++ header_footer.js
@@ -2,35 +2,19 @@ var element = document.getElementById('body-table');
 // Header
 element.insertAdjacentHTML('afterbegin', `<tr>
 			<td height=128 width=128>
-				<img src="./images/turtleisdot.jpg"><br>
+				<a href="./index.html"><img src="./images/turtleisdot.jpg"></a><br>
 			</td>
 			<td height=128 width=741>
-				<img src="./images/tisbanner1.jpg"><br>
+				<a href="./index.html"><img src="./images/tisbanner1.jpg"></a><br>
 			</td>
 			<td height=128 width=128>
-				<img src="./images/turtleisdot.jpg"><br>
+				<a href="./index.html"><img src="./images/turtleisdot.jpg"></a><br>
 			</td>
 		</tr>`);
 // Footer
-element.insertAdjacentHTML('beforeend', `	 	<tr>
-	    	<td valign="top" colspan=3 bgcolor=#FFFFFF>
-				<table cellpadding=0 cellspacing=0>
-					<tr>
-						<td>
-							<img src="./images/cor_tl.gif" width=10 height=10><br>
-						</td>
-						<td>
-							<img src="./images/cor_t.gif" width=977 height=10><br>
-						</td>
-						<td>
-							<img src="./images/cor_tr.gif" width=10 height=10><br>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<img src="./images/cor_l.gif" width=10 height=120><br>
-						</td>
-						<td>
+element.insertAdjacentHTML('beforeend', `<tr>
+	    	<td colspan=3 class="footer-cell">
+				
 						    <font size="1">This site is best viewed in 1024x768 resolution or higher. All content on this site is <font color="#008000">my</font> intellectual 
 						    property and may not be used without <font color="#008000">my</font> permission. <font color="#ff0000">CrazyBoy</font> and 
 						    <font color="#ff00ff">Eddie da Pig</font> are merely figments of my imagination, please do not take anything they type seriously. No animals or 
@@ -40,22 +24,6 @@ element.insertAdjacentHTML('beforeend', `	 	<tr>
 						    while you are here. You might want to look behind you right now to make sure no one is trying to sneak up on you. Even though snacking during 
 						    your viewing of this site could cause you to become overweight, it will also make you happy. Did you ever notice how clouds sometimes look like 
 						    other things? Okay, I'm done now.<br></font>
-			    		</td>
-			    		<td>
-			    			<img src="./images/cor_r.gif" width=10 height=120><br>
-			    		</td>
-			    	</tr>
-			    	<tr>
-			    		<td>
-			    			<img src="./images/cor_bl.gif" width=10 height=10><br>
-			    		</td>
-			    		<td>
-			    			<img src="./images/cor_b.gif" width=977 height=10><br>
-			    		</td>
-			    		<td>
-			    			<img src="./images/cor_br.gif" width=10 height=10><br>
-			    		</td>
-			    	</tr>
-			    </table>
+			    		
 			</td>
 		</tr>`);
