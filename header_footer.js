663394e790f479f570a68877452a3e68982ff798
Blaise Alicki <blaise@alicki.com>
2025-05-14 20:23:18 -0400

    Now with 80% fewer tables

--- header_footer.js
+++ header_footer.js
@@ -1,27 +1,19 @@
-var element = document.getElementById('body-table');
+var element = document.getElementById('body');
 // Header
-element.insertAdjacentHTML('afterbegin', `<tr>
-			<td height=128 width=128>
-			</td>
-			<td height=128 width=741>
-				<a href="./index.html" class="header">TURTLE INTERACTIVE SOFTWARE</a><br>
-			</td>
-			<td height=128 width=128>
-			</td>
-		</tr>`);
+element.insertAdjacentHTML('afterbegin', 
+	`<header>
+		<a href="./index.html" class="header">TURTLE INTERACTIVE SOFTWARE</a><br>
+	</header>`);
 // Footer
-element.insertAdjacentHTML('beforeend', `<tr>
-	    	<td colspan=3 class="footer-cell">
-				
-						    <font size="1">This site is best viewed in 1024x768 resolution or higher. All content on this site is <font color="#008000">my</font> intellectual 
-						    property and may not be used without <font color="#008000">my</font> permission. <font color="#ff0000">CrazyBoy</font> and 
-						    <font color="#ff00ff">Eddie da Pig</font> are merely figments of my imagination, please do not take anything they type seriously. No animals or 
-						    aliens were harmed during the making of the site. If you would like to stop seeing this site feel free to close your eyes, just don't expect a 
-						    refund. I can't be held responsible for anything you see when you blink, even if you were looking at my site immediately before and after you 
-						    engaged in the act of blinking. If you usually wear a clown nose, your enjoyment of this site will be significantly enhanced if you take it off 
-						    while you are here. You might want to look behind you right now to make sure no one is trying to sneak up on you. Even though snacking during 
-						    your viewing of this site could cause you to become overweight, it will also make you happy. Did you ever notice how clouds sometimes look like 
-						    other things? Okay, I'm done now.<br></font>
-			    		
-			</td>
-		</tr>`);
+element.insertAdjacentHTML('beforeend', 
+	`<footer>				
+		This site is best viewed in 1024x768 resolution or higher. All content on this site is <span class="blaise">my</span> intellectual 
+		property and may not be used without <span class="blaise">my</span> permission. <span class="crazyboy">CrazyBoy</span> and 
+		<span class="eddie">Eddie da Pig</span> are merely figments of my imagination, please do not take anything they type seriously. No animals or 
+		aliens were harmed during the making of the site. If you would like to stop seeing this site feel free to close your eyes, just don't expect a 
+		refund. I can't be held responsible for anything you see when you blink, even if you were looking at my site immediately before and after you 
+		engaged in the act of blinking. If you usually wear a clown nose, your enjoyment of this site will be significantly enhanced if you take it off 
+		while you are here. You might want to look behind you right now to make sure no one is trying to sneak up on you. Even though snacking during 
+		your viewing of this site could cause you to become overweight, it will also make you happy. Did you ever notice how clouds sometimes look like 
+		other things? Okay, I'm done now.<br>			    		
+	</footer>`);
