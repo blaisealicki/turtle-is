92015901ebaaf0f00be762395b6ff46569b3ffc9
Blaise Alicki <blaise@alicki.com>
2025-05-20 19:36:25 -0400

    Remove the old javascript menu, replace with new css menu.

--- header_footer.js
+++ header_footer.js
@@ -1,4 +1,62 @@
 var element = document.getElementById('body');
+// Menu
+element.insertAdjacentHTML('afterbegin', 
+	`<ul class="nav">
+		<li>Site
+			<ul>
+				<li><a href="./index.html">Home Page</a></li>
+				<li><a href="./updates.html">Updates</a></li>
+			</ul>
+		</li>
+		<li>Me
+			<ul>
+				<li><a href="./about-me.html">About Me</a></li>
+				<li><a href="./my-dvds.html">My DVDs</a></li>
+				<li><a href="./ramblings.html">Ramblings</a></li>
+				<li><a href="./im-too-tired.html">I'm Too Tired</a></li>
+			</ul>
+		</li>
+		<li>Art
+			<ul>
+				<li><a href="./pictures.html">Pictures</a></li>
+				<li><a href="./drawings.html">Drawings</a></li>
+				<li><a href="./colors.html">Colors</a></li>
+				<li><a href="./pixels.html">Pixels</a></li>
+				<li><a href="./comics.html">Comics</a></li>
+				<li><a href="./ninjahc.html">Comic Book</a></li>
+				<li><a href="https://localandhost.com/comic/first-day/" target="_blank">Local And Host</a></li>
+				<li><a href="https://goblinsofstinkmarsh.com/comic/gore-duty/" target="_blank">Goblins</a></li>
+			</ul>
+		</li>
+		<li>Other Works
+			<ul>
+				<li><a href="./ode-to-my-pop.html">Ode To My Pop</a></li>
+				<li><a href="./program-journal.html">Program Journal</a></li>
+				<li><a href="./blaise01.mid">Midi Song</a></li>
+			</ul>
+		</li>
+		<li>Projects
+			<ul>
+				<li><a href="./brilliant-ideas.html">Brilliant Ideas</a></li>
+				<li><a href="./i-show.html">i-Show</a></li>
+				<li><a href="./movies.html">Movies</a></li>
+				<li><a href="./game.html">Game</a></li>
+			</ul>
+		</li>
+		<li>Kmart
+			<ul>
+				<li><a href="./kmart.html">Kmart</a></li>
+				<li><a href="./kmart-pics.html">Kmart Pics</a></li>
+			</ul>
+		</li>
+		<li>Interactive
+			<ul>
+				<li><a href="./wallpapers.html">Wallpaper</a></li>
+				<li><a href="./links.html">Links</a></li>
+				<li><a href="https://blaise.alicki.com" target="_blank">Blog</a></li>
+			</ul>
+		</li>
+	</ul>`);
 // Header
 element.insertAdjacentHTML('afterbegin', 
 	`<header>
