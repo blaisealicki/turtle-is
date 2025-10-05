ea31f5cc9e9130a54558fc2c8b5f2683352fae0d
Blaise Alicki <blaise@alicki.com>
2025-10-05 19:53:21 -0400

    Add comics

--- comic/header_footer.js
+++ comic/header_footer.js
@@ -66,13 +66,7 @@ element.insertAdjacentHTML('afterbegin',
 // Footer
 element.insertAdjacentHTML('beforeend', 
 	`<footer>				
-		This site is best viewed in 1024x768 resolution or higher. All content on this site is <span class="blaise">my</span> intellectual 
-		property and may not be used without <span class="blaise">my</span> permission. <span class="crazyboy">CrazyBoy</span> and 
-		<span class="eddie">Eddie da Pig</span> are merely figments of my imagination, please do not take anything they type seriously. No animals or 
-		aliens were harmed during the making of the site. If you would like to stop seeing this site feel free to close your eyes, just don't expect a 
-		refund. I can't be held responsible for anything you see when you blink, even if you were looking at my site immediately before and after you 
-		engaged in the act of blinking. If you usually wear a clown nose, your enjoyment of this site will be significantly enhanced if you take it off 
-		while you are here. You might want to look behind you right now to make sure no one is trying to sneak up on you. Even though snacking during 
-		your viewing of this site could cause you to become overweight, it will also make you happy. Did you ever notice how clouds sometimes look like 
-		other things? Okay, I'm done now.<br>			    		
+		<div style="margin-left: auto; margin-right: auto; width: 330px;">
+			<button onclick="first()">&lt;&lt;</button> <button onclick="prev()">&lt;</button> <span id="comic-date" style="display: inline-block; width: 200px; text-align: center;"></span> <button onclick="next()">&gt;</button> <button onclick="last()">&gt;&gt;</button>
+		</div>  		
 	</footer>`);
